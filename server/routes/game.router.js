a7cd466b2e119438cd320a691c3d810b7f716bd3
David Vetter <ddvetter23@gmail.com>
2022-10-20 00:28:03 -0500

    added POST to add a new game and the individual player stats from that game

---
+++ server/routes/game.router.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const router = express.Router();
+
+// POST to add a new game, then add player stats with new game_id
+router.post('/', rejectUnauthenticated, (req, res) => {
+    console.log('This is req.body in POST: ', req.body);
+    const teamId = req.body.teamId;
+    const opponent = req.body.opponent;
+    const isWinner = req.body.isWinner;
+    const scoreHomeTeam = req.body.scoreHomeTeam;
+    const scoreAwayTeam = req.body.scoreAwayTeam;
+    const innings = req.body.innings;
+    const isHomeTeam = req.body.isHomeTeam;
+    // POST route code here
+    const query = `INSERT INTO "game" 
+                    (
+                    "team_id", 
+                    "opponent", 
+                    "is_winner", 
+                    "score_home_team", 
+                    "score_away_team", 
+                    "innings", 
+                    "is_home_team"
+                    )
+                   VALUES ($1, $2, $3, $4, $5, $6, $7)
+                   RETURNING "id";`;
+    pool.query(query, 
+                [
+                    teamId, 
+                    opponent, 
+                    isWinner, 
+                    scoreHomeTeam, 
+                    scoreAwayTeam, 
+                    innings, 
+                    isHomeTeam
+                ])
+        .then(result => {
+            // This query will add each user's game stats to user_game table
+            // players are in array of objects
+            // loop through each player in array and run INSERT for
+            // each one
+            console.log('this is the new game id: ', result.rows[0].id);
+            console.log('this is playerArray: ', req.body.playerArray);
+            for (let player of req.body.playerArray) {
+            const userId = player.userId;
+            const gameId = result.rows[0].id;
+            const hits = player.hits;
+            const walks = player.walks;
+            const atBats = player.atBats;
+            const rbi = player.rbi;
+            const strikeouts = player.strikeouts;
+            const position = player.position;
+            const lineupNumber = player.lineupNumber;
+            const single = player.single;
+            const double = player.double;
+            const triple = player.triple;
+            const hr = player.hr;
+            const userQuery = `INSERT INTO "user_game" 
+                            (
+                            "user_id", 
+                            "game_id", 
+                            "hits", 
+                            "walks", 
+                            "at_bats",
+                            "rbi",
+                            "strikeouts",
+                            "position",
+                            "lineup_number",
+                            "single",
+                            "double",
+                            "triple",
+                            "hr"
+                            ) 
+                            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);`;
+            pool.query(userQuery, 
+                [
+                    userId, 
+                    gameId, 
+                    hits, 
+                    walks, 
+                    atBats, 
+                    rbi, 
+                    strikeouts, 
+                    position, 
+                    lineupNumber, 
+                    single, 
+                    double, 
+                    triple, 
+                    hr
+                ])
+            .then(result => {
+                console.log(`User ${userId} had game ${gameId} added!`);
+            })
+            .catch(err => {
+                console.log('error in adding user after game add: ', err);
+                res.sendStatus(500);
+            })
+            }
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('Error in POST new game: ', err);
+            res.sendStatus(500);
+        })
+  }); // End POST
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const playerRouter = require('./routes/player.router');
 const teamRouter = require('./routes/team.router');
+const gameRouter = require('./routes/game.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -27,6 +28,7 @@ app.use(passport.session());
 app.use('/api/user', userRouter);
 app.use('/api/player', playerRouter);
 app.use('/api/team', teamRouter);
+app.use('/api/game', gameRouter);
 
 // Serve static files
 app.use(express.static('build'));
